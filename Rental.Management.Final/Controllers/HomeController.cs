32c232a25b732c4b9e06bffa4dd7f19136c2ceef
Johnny Arnett <im10g@hotmail.com>
2024-04-12 12:40:08 -0400

    Home Page Improvements

--- Rental.Management.Final/Controllers/HomeController.cs
+++ Rental.Management.Final/Controllers/HomeController.cs
@@ -1,21 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Rental.Management.Final.Models;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Rental.Management.Final.Data;
+using Rental.Management.Final.Views.Home;
+using Rental.Management.Final.Views.RentalProperties;
 
 namespace Rental.Management.Final.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var properties = await _context.RentalProperties.ToListAsync();
+
+            List<PropertyVm> propertiesVms = new List<PropertyVm>();
+
+            foreach (var property in properties)
+            {
+                var propertyImages = await _context.PropertyImages.Where(p => p.PropertyId.Equals(property.Id)).ToListAsync();
+                List<byte[]> images = new List<byte[]>();
+                foreach (var image in propertyImages)
+                {
+                    images.Add(image.Image);
+                }
+
+                var vm = new PropertyVm()
+                {
+                    Id = property.Id,
+                    Address = property.Address,
+                    Description = property.Description,
+                    Price = property.Price,
+                    IsOccupied = property.IsOccupied,
+                    Customers = property.Customers,
+                    PropertyImages = images,
+                };
+
+                propertiesVms.Add(vm);
+            }
+
+            var IndexVm = new HomeVm()
+            {
+                DisplayProperties = propertiesVms
+            };
+            return View(IndexVm);
         }
 
         public IActionResult Privacy()
---
+++ Rental.Management.Final/Views/Home/HomeVm.cs
@@ -0,0 +1,9 @@
+﻿using Rental.Management.Final.Views.RentalProperties;
+
+namespace Rental.Management.Final.Views.Home
+{
+    public class HomeVm
+    {
+        public List<PropertyVm> DisplayProperties { get; set; }
+    }
+}
