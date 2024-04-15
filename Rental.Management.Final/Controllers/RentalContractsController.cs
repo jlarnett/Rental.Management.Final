08203ae0c4eb2db65b7634e605597634717f354c
Johnny Arnett <im10g@hotmail.com>
2024-04-15 16:42:52 -0400

    Got the rental contract payment system handled. Contracts are updated when the payment information is entered. We should now have the information to verify whether a property is occupied during a specific period or not.

--- Rental.Management.Final/Controllers/RentalContractsController.cs
+++ Rental.Management.Final/Controllers/RentalContractsController.cs
@@ -28,10 +28,6 @@ namespace Rental.Management.Final.Controllers
             return View(contract);
         }
 
-        // POST: RentalProperties/Create
-
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ContractId,StartDate,EndDate,PropertyId,CustomerId")] RentalContract contract)
@@ -39,20 +35,56 @@ namespace Rental.Management.Final.Controllers
 
             if (ModelState.IsValid)
             {
-                await _context.AddAsync(contract);
+                var newContract = await _context.AddAsync(contract);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(CreateContractPayment));
+
+
+                var property = await _context.RentalProperties.FindAsync(contract.PropertyId);
+                var totalDaysForContract = contract.EndDate.Subtract(contract.StartDate).Days;
+                var totalContractAmount = totalDaysForContract * property.Price;
+
+                return RedirectToAction(nameof(CreateContractPayment), new {id=newContract.Entity.ContractId, numberOfDaysReserved=totalDaysForContract, propertyPrice=property.Price});
             }
             return View(contract);
         }
 
         // GET: RentalContracts
-        public IActionResult CreateContractPayment(int id)
+        public IActionResult CreateContractPayment(int id, int numberOfDaysReserved, double propertyPrice)
         {
+            ViewBag.DaysInContract = numberOfDaysReserved;
+            ViewBag.PropertyPrice = propertyPrice;
+
             return View("Payment", new ContractPayment()
             {
-                Date = DateTime.UtcNow
+                Date = DateTime.UtcNow,
+                Amount = propertyPrice * numberOfDaysReserved,
+                ContractId = id
             });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateContractPayment([Bind("CardHolderName,CardType,CardNumber,CardExpMonth,CardExpYear,Amount,ContractId,Date")] ContractPayment payment)
+        {
+
+            if (ModelState.IsValid)
+            {
+                await _context.AddAsync(payment);
+                var rowsModified = await _context.SaveChangesAsync();
+
+                if (rowsModified > 0)
+                {
+                    var contract = await _context.RentalContracts.FindAsync(payment.ContractId);
+
+                    if (contract != null)
+                    {
+                        contract.PaymentReceived = true;
+                        await _context.SaveChangesAsync();
+                    }
+                }
+                return RedirectToAction(nameof(CreateContractPayment));
+            }
+            return View(payment);
         }
     }
 
